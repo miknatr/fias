3678129fd8bb5ea4a5f7bb2dd06d21196e5092cf
apolev <alex.v.polev@gmail.com>
2014-01-01 02:33:07 +0400

    IS-1042 Проверка обновлений на сайте, загрузка файлов: [mod] при сохранении проверяем, есть ли уже докачанный файл такой же версии.

--- src/Fias/InitLoader.php
+++ src/Fias/InitLoader.php
@@ -6,6 +6,8 @@ class InitLoader extends Loader
 {
     public function loadFile()
     {
-
+        $filesInfo = $this->getLastFileInfo();
+        $fileName  = $this->generateFileName($filesInfo, 'FiasCompleteXmlUrl');
+        return $this->loadFileFromUrl($fileName, $filesInfo->GetLastDownloadFileInfoResult->FiasCompleteXmlUrl);
     }
 }
