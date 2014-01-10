a18df2c44c4fb212a4aabe3ff2cbc5b306c07c85
apolev <alex.v.polev@gmail.com>
2014-01-10 11:47:08 +0400

    Реализация XmlReader, тесты под него.

--- src/Fias/Tests/XmlReaderTest.php
+++ src/Fias/Tests/XmlReaderTest.php
@@ -6,5 +6,44 @@ use Fias\XmlReader;
 
 class XmlReaderTest extends \PHPUnit_Framework_TestCase
 {
-    
+    /** @var XMLReader */
+    private $reader;
+
+    protected function setUp()
+    {
+        $this->reader = new XMLReader(
+            __DIR__ . '/resources/readerTest.xml',
+            'Computer',
+            array(
+                'id',
+                'madeIn',
+                'fakeAttribute'
+        ));
+    }
+
+    public function testRead()
+    {
+        $rows = $this->reader->getRows();
+
+        $this->assertEquals(2, count($rows));
+        $this->assertEquals('USA', $rows[1]['madeIn']);
+        $this->assertEquals(null, $rows[0]['fakeAttribute']);
+        $this->assertTrue(!isset($rows[0]['title']));
+    }
+
+    public function testReadWithCount()
+    {
+        $this->assertEquals(1, count($this->reader->getRows(1)));
+        $this->assertEquals(1, count($this->reader->getRows(1)));
+        $this->assertEquals(0, count($this->reader->getRows(1)));
+    }
+
+    /**
+     * @expectedException \LogicException
+     * @expectedExceptionMessage количества
+     */
+    public function testBadCount()
+    {
+        $this->reader->getRows(0);
+    }
 }
--- src/Fias/XmlReader.php
+++ src/Fias/XmlReader.php
@@ -4,6 +4,7 @@ namespace Fias;
 
 class XmlReader
 {
+    /** @var \XMLReader */
     private $reader;
     private $nodeName;
     private $attributes = array();
@@ -16,11 +17,6 @@ class XmlReader
         $this->initializeReader($pathToFile);
     }
 
-    public function getRows($count = 1000)
-    {
-        //STOPPER Тесты и реализация функции
-    }
-
     private function initializeReader($pathToFile)
     {
         FileHelper::ensureIsReadable($pathToFile);
@@ -28,9 +24,39 @@ class XmlReader
         $this->reader = new \XMLReader();
 
         $success = $this->reader->open($pathToFile);
-        if (!$success)
-        {
+        if (!$success) {
             throw new ImporterException('Ошибка открытия XML файла по адресу: ' . $pathToFile);
         }
+    }
+
+    public function getRows($maxCount = 1000)
+    {
+        if ($maxCount < 1) {
+            throw new \LogicException('Неверное значение максимального количества строк: ' . $maxCount);
+        }
+
+        $result = array();
+        $count  = 0;
+
+        while ($this->reader->read() && ($count < $maxCount)) {
+            if ($this->reader->name == $this->nodeName) {
+                $result[] = $this->getRowAttributes();
+                ++$count;
+            }
+        }
+
+        return $result;
+    }
+
+    private function getRowAttributes()
+    {
+        $result = array();
+
+        foreach ($this->attributes as $attribute) {
+            // Если атрибут отсутствует, в $result[$attribute] окажется null.
+            $result[$attribute] = $this->reader->getAttribute($attribute);
+        }
+
+        return $result;
     }
 }
