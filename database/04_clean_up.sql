99823025019926828ec2da18a79f51d92532e453
apolev <alex.v.polev@gmail.com>
2014-01-28 14:58:01 +0400

    Поправил имя индекса в кластере.

--- database/04_clean_up.sql
+++ database/04_clean_up.sql
@@ -5,7 +5,7 @@ DROP INDEX "tmp_houses_building_id_fkey_idx";
 DROP INDEX "tmp_houses_structure_fkey_idx";
 
 
-CLUSTER houses USING houses_full_number_id_fkey_idx;
+CLUSTER houses USING houses_full_number_idx;
 ANALYZE;
 
 COMMIT;
