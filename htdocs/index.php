14c7d3e3dc174c42c99fd79f28a8e2813df13041
apolev <alex.v.polev@gmail.com>
2014-01-28 17:23:55 +0400

    пролезло пара строк после мерджа

--- htdocs/index.php
+++ htdocs/index.php
@@ -13,7 +13,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $config = Config::get('config');
 $db     = ConnectionFactory::getConnection($config->getParam('database'));
 $log    = new Logger('http');
-$log    = new Logger('general');
 $log->pushHandler(new StreamHandler(__DIR__ . 'logs/http.log'));
 
 
@@ -31,8 +30,6 @@ try {
         default:
             throw new \Exception($e->getMessage(), $e->getCode(), $e);
     }
-
-    RequestHandler::handle($_SERVER['REQUEST_URI'], $db);
 } catch (\Exception $e) {
     header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
     $log->addError($e->getMessage());
