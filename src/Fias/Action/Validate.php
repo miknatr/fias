5b513f7ace856176a74628dee103e6e3f9c26f58
apolev <alex.v.polev@gmail.com>
2014-01-29 15:02:56 +0400

    валидация

--- src/Fias/Action/Validate.php
+++ src/Fias/Action/Validate.php
@@ -26,54 +26,72 @@ class Validate
     private function parseAddress()
     {
         $tmp = explode(',', $this->address);
-        foreach ($tmp as $key => $part) {
-            $tmp[$key] = rtrim($part);
-        }
 
         return array(
-            'address' => implode(', ', $tmp),
-            'house'   => array_pop($tmp),
+            'house'   => trim(array_pop($tmp)),
+            'address' => implode(',', $tmp),
+            'level'   => count($tmp) - 1,
         );
     }
 
     private function find($address)
     {
         $result = array(
-            'is_valid'   => false,
-            'incomplete' => true,
-            'id'         => null,
+            'is_valid'    => false,
+            'is_complete' => false,
+            'id'          => null,
         );
 
-        $addressId = $this->findAddress($address['address']);
+        if ($address['level'] > 0) {
+            $addressId = $this->findAddress($address['address'], $address['level']);
 
-        if ($addressId) {
-            $houseId = $this->findHouse($addressId, $address['house']);
-            if ($houseId) {
-                $result['is_valid']    = true;
-                $result['is_complete'] = true;
-                $result['id']          = $houseId;
-            }
-        } else {
-            $incompleteAddressId = $this->findAddress($this->address);
-            if ($incompleteAddressId) {
-                $result['is_valid'] = true;
-                $result['id']       = $incompleteAddressId;
-                return $result;
+            if ($addressId) {
+                $houseId = $this->findHouse($addressId, $address['house']);
+                if ($houseId) {
+                    $result['is_valid']    = true;
+                    $result['is_complete'] = true;
+                    $result['id']          = $houseId;
+
+                    return $result;
+                }
             }
         }
 
+        $incompleteAddressId = $this->findAddress($this->address, $address['level'] + 1);
+        if ($incompleteAddressId) {
+            $result['is_valid'] = true;
+            $result['id']       = $incompleteAddressId;
+
+            return $result;
+        }
 
         // Ничего не нашлось
         return $result;
     }
 
-    private function findAddress($address)
+    private function findAddress($address, $level)
     {
-        return null;
+        $sql = 'SELECT address_id
+                FROM address_objects
+                WHERE level = ?q
+                    AND lower(full_title) = lower(?q)'
+        ;
+
+        $result = $this->db->execute($sql, array($level, $address))->fetchOneOrFalse();
+
+        return $result ? $result['address_id'] : false;
     }
 
-    private function findHouse($parentId, $house)
+    private function findHouse($addressId, $house)
     {
-        return null;
+        $sql = 'SELECT home_id
+                FROM houses
+                WHERE address_id = ?q
+                    AND full_number = lower(?q)'
+        ;
+
+        $result = $this->db->execute($sql, array($addressId, $house))->fetchOneOrFalse();
+
+        return $result ? $result['home_id'] : false;
     }
 }
