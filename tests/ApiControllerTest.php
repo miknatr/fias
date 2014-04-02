9062b84c256e9b81cc7d78d7fc5a54df341b5242
apolev <alex.v.polev@gmail.com>
2014-04-02 18:47:50 +0400

    Поправил тесты.

--- tests/ApiControllerTest.php
+++ tests/ApiControllerTest.php
@@ -3,32 +3,53 @@
 use Browser\TestBrowserTrait;
 use ApiAction\Completion;
 
-class ApiControllerTest extends \PHPUnit_Framework_TestCase
+class ApiControllerTest extends MockDatabaseTest
 {
     use TestBrowserTrait;
 
-    /** @var Container */
-    private $container;
-
     public function setUp()
     {
-        $this->container = new Container();
-        $this->prepareHttpClient('http://' . $this->container->getHost());
+        parent::setUp();
+        $this->prepareHttpClient('http://' . Helper::getContainer()->getHost());
     }
 
     public function testComplete()
     {
         $tooBigLimit = Completion::MAX_LIMIT + 1;
-        $this->loadPage('/api/complete/?address=Москва&limit=' . $tooBigLimit, 400);
+        $this->describe('Проверяем отсечение слишком большого лимита.')
+            ->loadPage('/api/complete/?address=Москва&limit=' . $tooBigLimit, 400)
+            ->describe('Проверяем автоподстановку для адресного объекта.')
+            ->loadPage('/api/complete/?address=Моск', 200)
+            ->ensureResponse(json_decode($this->curResponse->getContent())->addresses[0]->title == 'г Москва')
+            ->describe('Проверяем автоподстановку для дома.')
+            ->loadPage('/api/complete/?address=г Москва, ул Стахановская, 1')
+            ->ensureResponse(json_decode($this->curResponse->getContent())->addresses[0]->title == 'г Москва, ул Стахановская, 16с1')
+        ;
     }
 
     public function testValidate()
     {
-        $this->loadPage('/api/validate/', 400);
+        $this->describe('Проверяем выдачу ошибки при отсутствии необходимого параметра')
+            ->loadPage('/api/validate/', 400)
+            ->describe('Проверяем валидацию корректного адресного объекта')
+            ->loadPage('/api/validate/?address=г Москва', 200)
+            ->ensureResponse(json_decode($this->curResponse->getContent())->is_valid && !json_decode($this->curResponse->getContent())->is_complete)
+            ->describe('Проверяем валидацию некорректного адресного объекта')
+            ->loadPage('/api/validate/?address=г Мосва', 200)
+            ->ensureResponse(!json_decode($this->curResponse->getContent())->is_valid && !json_decode($this->curResponse->getContent())->is_complete)
+            ->describe('Проверяем валидацию корректного дома')
+            ->loadPage('/api/validate/?address=г Москва, ул Стахановская, 16с1', 200)
+            ->ensureResponse(json_decode($this->curResponse->getContent())->is_valid && json_decode($this->curResponse->getContent())->is_complete)
+            ->describe('Проверяем валидацию некорректного дома')
+            ->loadPage('/api/validate/?address=г Москва, ул Стахановская, 1324326с1', 200)
+            ->ensureResponse(!json_decode($this->curResponse->getContent())->is_valid && !json_decode($this->curResponse->getContent())->is_complete)
+        ;
     }
 
     public function testNotFound()
     {
-        $this->loadPage('/totally/wrong/destination', 404);
+        $this->describe('Проверяем выдачу 404 ошибки при ошибочном uri')
+            ->loadPage('/totally/wrong/destination', 404)
+        ;
     }
 }
